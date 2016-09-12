8c96fd3dd1ffe47d405ec0c1056e00f57c790a31
Luis Lara <luislarap@outlook.com>
2016-09-12 15:06:07 -0500

    Define constructor for NeuralNetwork class

--- robot_nsga/neuralnet/neural_network.py
+++ robot_nsga/neuralnet/neural_network.py
@@ -2,4 +2,7 @@
 
 class NeuralNetwork:
 	'''Modular neural network, built as a stack of layers'''
-	pass
+
+	def __init__(self):
+		'''Creates an empty, unusable neural network'''
+		self.layers = []
