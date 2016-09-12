c83bea933bb49891ea8a8bda3a669261bf40ac11
Luis Lara <luislarap@outlook.com>
2016-09-12 18:47:42 -0500

    Create constructor for FullyConnectedLayer
    Add the option for no activation function.

--- robot_nsga/neuralnet/fully_connected_layer.py
+++ robot_nsga/neuralnet/fully_connected_layer.py
@@ -1,5 +1,23 @@
 '''Defines the FullyConnectedLayer class'''
 
+import numpy
+import theano
+import theano.tensor as T
+
 class FullyConnectedLayer:
 	'''Layer in which all input neurons connect to all outputs'''
-	pass
+
+	def __init__(self, n_in, n_out, activation=None):
+		'''Constructs a layer with n_in inputs, n_out outputs and the given activation function.
+
+		If the activation is None, it acts as a linear activation.
+		'''
+		self.input = T.dmatrix('x')
+		self.n_in = n_in
+		self.n_out = n_out
+		self.weights = theano.shared(numpy.zeros((n_out, n_in)), name='w')
+		self.bias = theano.shared(numpy.zeros((n_out, 1)), name='b')
+		if activation == None:
+			self.output = T.dot(self.weights, self.input) + self.bias
+		else:
+			self.output = activation(T.dot(self.weights, self.input) + self.bias)
