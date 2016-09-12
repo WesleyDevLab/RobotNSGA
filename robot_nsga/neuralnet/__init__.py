3428f9396c22110d36295e1fe50925fb9dd4e8f1
Luis Lara <luislarap@outlook.com>
2016-09-12 15:08:25 -0500

    Create class FullyConnectedLayer

--- robot_nsga/neuralnet/__init__.py
+++ robot_nsga/neuralnet/__init__.py
@@ -1,1 +1,2 @@
 from neural_network import NeuralNetwork
+from fully_connected_layer import FullyConnectedLayer
---
+++ robot_nsga/neuralnet/fully_connected_layer.py
@@ -0,0 +1,5 @@
+'''Defines the FullyConnectedLayer class'''
+
+class FullyConnectedLayer:
+	'''Layer in which all input neurons connect to all outputs'''
+	pass
