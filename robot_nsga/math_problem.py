062fafd513312ee4e9cea4fb48e7736fa0effa38
Luis Lara <luislarap@outlook.com>
2016-09-14 17:37:58 -0500

    Create problem class for the math module

--- robot_nsga/math_problem.py
+++ robot_nsga/math_problem.py
@@ -1,15 +1,45 @@
 '''Evolution of a neural network to perform simple math functions.'''
 
+import random
+
 import evolution
+import neuralnet
 from database import Database
+
+
+ARCHITECTURE = [2, 10, 10, 2]
 
 
 class MathProblem(evolution.Problem):
 	'''Problem class for this module'''
-	pass
+
+	def __init__(self):
+		'''Initializes parameters for the problem'''
+		self.n_params = 0
+		for i in range(len(ARCHITECTURE) - 1):
+			self.n_params += ARCHITECTURE[i] * ARCHITECTURE[i + 1] + ARCHITECTURE[i + 1]
+
+	def _create_network(self, chromosome=None):
+		'''Creates a neural network for this problem'''
+		network = neuralnet.NeuralNetwork()
+		for i in range(len(ARCHITECTURE) - 1):
+			network.add_layer(neuralnet.FullyConnectedLayer(ARCHITECTURE[i], ARCHITECTURE[i + 1]))
+		network.compile()
+		if chromosome is not None:
+			network.set_params(chromosome)
+
+	def generate_population(self, size):
+		'''Creates a new random population'''
+		new_population = evolution.Population()
+		for i in range(size):
+			name = 'I' + str(i)
+			chromosome = [random.gauss(0, 1)]
+			new_individual = evolution.Individual(chromosome)
+			new_population.individuals[name] = new_individual
 
 
 def main(args):
 	'''Module main method'''
+	random.seed()
 	database = Database(args.database)
 	problem = MathProblem()
