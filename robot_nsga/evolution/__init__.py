0c9b1cc6134eb99d0e33c4085343eae54bf47da0
Luis Lara <luislarap@outlook.com>
2016-09-14 15:02:06 -0500

    Create class Population

--- robot_nsga/evolution/__init__.py
+++ robot_nsga/evolution/__init__.py
@@ -1,2 +1,3 @@
 from .nsga import NSGA
 from .individual import Individual
+from .population import Population
---
+++ robot_nsga/evolution/population.py
@@ -0,0 +1,9 @@
+'''Defines the Population class'''
+
+
+class Population:
+	'''Group of Individuals for use in an iteration of a genetic algorithm'''
+
+	def __init__(self):
+		'''Creates an empty population'''
+		self.individuals = []
