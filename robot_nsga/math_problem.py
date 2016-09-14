1d140f64480d08e56e9495efd5fff0f154a6d3c8
Luis Lara <luislarap@outlook.com>
2016-09-14 18:39:08 -0500

    Fix math module
    Switch to numpy RNG, using numpy to obtain binary string and fix
    chromosome not saving.

--- robot_nsga/math_problem.py
+++ robot_nsga/math_problem.py
@@ -1,6 +1,6 @@
 '''Evolution of a neural network to perform simple math functions.'''
 
-import random
+import numpy
 
 import evolution
 import neuralnet
@@ -18,6 +18,7 @@ class MathProblem(evolution.Problem):
 		self.n_params = 0
 		for i in range(len(ARCHITECTURE) - 1):
 			self.n_params += ARCHITECTURE[i] * ARCHITECTURE[i + 1] + ARCHITECTURE[i + 1]
+		print(self.n_params)
 
 	def _create_network(self, chromosome=None):
 		'''Creates a neural network for this problem'''
@@ -33,13 +34,19 @@ class MathProblem(evolution.Problem):
 		new_population = evolution.Population()
 		for i in range(size):
 			name = 'I' + str(i)
-			chromosome = [random.gauss(0, 1)]
+			chromosome = [numpy.random.normal(0, 1) for _ in range(self.n_params)]
 			new_individual = evolution.Individual(chromosome)
 			new_population.individuals[name] = new_individual
+		return new_population
 
 
 def main(args):
 	'''Module main method'''
-	random.seed()
+	numpy.random.seed()
 	database = Database(args.database)
 	problem = MathProblem()
+	database.create_population()
+	first_population = problem.generate_population(10)
+	to_save = {key: numpy.asarray(val.chromosome).tobytes() for key, val in first_population.individuals.items()}
+	database.save(to_save)
+	print(database.load())
