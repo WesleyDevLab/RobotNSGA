5da774d91dfef8efb868100f1b5a37eba86fa062
Luis Lara <luislarap@outlook.com>
2016-09-14 14:24:41 -0500

    Create NSGA __init__ method

--- robot_nsga/evolution/nsga.py
+++ robot_nsga/evolution/nsga.py
@@ -2,4 +2,11 @@
 
 class NSGA:
 	'''Multi-objective genetic algorithm'''
-	pass
+
+	def __init__(self, population, children=None):
+		'''Creates a genetic algorithm that will work on the provided population and children.
+
+		If children are not provided, the algorithm will behave as if it was its first iteration.
+		'''
+		self.population = population
+		self.children = children
