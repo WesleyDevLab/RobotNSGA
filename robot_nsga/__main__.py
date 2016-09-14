8f098f2fc4ad2a63a5cb603c404e296b64136356
Luis Lara <luislarap@outlook.com>
2016-09-14 12:17:15 -0500

    Add command line parsing
    Create the math subcommand.

--- robot_nsga/__main__.py
+++ robot_nsga/__main__.py
@@ -1,16 +1,28 @@
 '''Entry point for the application'''
 
-import sys
+import argparse
+
+import math_problem
+
 
 def main():
 	'''Application main function'''
-	module = sys.argv[1]
-	args = sys.argv[2:]
-	if module == 'math':
-		import math_problem
-		math_problem.main(args)
-	else:
-		print('Module not found')
+	global_parser = argparse.ArgumentParser()
+	global_parser.add_argument('-d', '--database', help='The directory to be used as database')
+	subparsers = global_parser.add_subparsers(title='subcommands')
+
+	# Math problem
+	math_parser = subparsers.add_parser('math',
+		description='Train neural networks to perform simple mathematical functions',
+		help='Execute the math test')
+	math_parser.set_defaults(
+		database='MathDatabase',
+		func=math_problem.main
+		)
+
+	args = global_parser.parse_args()
+	args.func(args)
+
 
 if __name__ == '__main__':
 	main()
--- robot_nsga/math_problem.py
+++ robot_nsga/math_problem.py
@@ -1,15 +1,6 @@
 '''Evolution of a neural network to perform simple math functions.'''
 
-import theano.tensor as T
-
-import neuralnet
-
 
 def main(args):
 	'''Module main method'''
-	nn = neuralnet.NeuralNetwork()
-	nn.add_layer(neuralnet.FullyConnectedLayer(3, 3, T.nnet.sigmoid))
-	nn.add_layer(neuralnet.FullyConnectedLayer(3, 2, T.nnet.sigmoid))
-	nn.compile()
-	print(nn.set_params([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3]))
-	print(nn.predict([[-1], [0], [1]]))
+	pass
