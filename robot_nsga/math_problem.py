c5eb3c7da09f6ff3774b9cc618bb96afe8c1e588
Luis Lara <luislarap@outlook.com>
2016-09-25 18:17:00 -0500

    Reset math module

--- robot_nsga/math_problem.py
+++ robot_nsga/math_problem.py
@@ -1,12 +1,7 @@
 '''Evolution of a neural network to perform simple math functions.'''
 
-import random
-
-import numpy
-
 import evolution
 import neuralnet
-from database import Database
 
 
 ARCHITECTURE = [2, 10, 10, 2]
@@ -30,24 +25,7 @@ class MathProblem(evolution.Problem):
 		if chromosome is not None:
 			network.set_params(chromosome)
 
-	def generate_population(self, size):
-		'''Creates a new random population'''
-		new_population = evolution.Population()
-		for i in range(size):
-			name = 'I' + str(i)
-			chromosome = [random.gauss(0, 1) for _ in range(self.n_params)]
-			new_individual = evolution.Individual(chromosome)
-			new_population.individuals[name] = new_individual
-		return new_population
-
 
 def main(args):
 	'''Module main method'''
-	random.seed()
-	database = Database(args.database)
-	problem = MathProblem()
-	database.create_population()
-	first_population = problem.generate_population(10)
-	to_save = {key: numpy.asarray(val.chromosome).tobytes() for key, val in first_population.items()}
-	database.save(to_save)
-	print(database.load())
+	pass
