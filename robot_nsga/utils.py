7fd02e0e4fb4752791fba1015b619a9d273b9929
Luis Lara <luislarap@outlook.com>
2016-09-26 15:19:06 -0500

    Create class ProgressBar

--- robot_nsga/utils.py
+++ robot_nsga/utils.py
@@ -1,6 +1,19 @@
-'''Utility functions'''
+'''Utility functions and classes'''
 
 import numpy as np
+
+
+class ProgressBar:
+	'''Progress bar to be displayed in a separate line in the terminal'''
+
+	def __init__(self, width=79):
+		'''Initializes an empty progress bar, with the given width in characters
+
+		This method should be called when the cursor is in an empty line, as it will be overwritten.
+		'''
+		self.progress = 0
+		self.width = width
+
 
 def save_data(genetic_algorithm, database):
 	'''Saves relevant data after each iteration'''
