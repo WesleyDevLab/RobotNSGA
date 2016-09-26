f11db74fb4665c6b6c96c5abd22057e7d5327c3c
Luis Lara <luislarap@outlook.com>
2016-09-26 12:22:54 -0500

    Fix save_data method not saving chromosomes

--- robot_nsga/utils.py
+++ robot_nsga/utils.py
@@ -5,5 +5,6 @@ import numpy as np
 def save_data(genetic_algorithm, database):
 	'''Saves relevant data after each iteration'''
 	database.create_population()
-	save_dict = {val.name: np.asarray(val).tobytes() for val in genetic_algorithm.population}
+	save_dict = {val.name: np.asarray(val.chromosome).tobytes()
+		for val in genetic_algorithm.population}
 	database.save(save_dict)
