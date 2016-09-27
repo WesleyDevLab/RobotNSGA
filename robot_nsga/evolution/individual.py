166641cafb1d8f74e329d26eeec7e98dbdf3b08e
Luis Lara <luislarap@outlook.com>
2016-09-27 13:17:42 -0500

    Implement < and > comparison operators in Individual

--- robot_nsga/evolution/individual.py
+++ robot_nsga/evolution/individual.py
@@ -14,6 +14,18 @@ class Individual:
 		self.domination_count = 0
 		self.dominated_solutions = []
 
+	def __gt__(self, other):
+		if self.rank == other.rank:
+			return self.crowding_distance > other.crowding_distance
+		else:
+			return self.rank < other.rank
+
+	def __lt__(self, other):
+		if self.rank == other.rank:
+			return self.crowding_distance < other.crowding_distance
+		else:
+			return self.rank > other.rank
+
 	def __repr__(self):
 		fitness_repr = ['{:0.2f}'.format(a) for a in self.fitness]
 		return self.name + '(' + str(fitness_repr) + ', ' + str(self.rank) + ', ' + \
