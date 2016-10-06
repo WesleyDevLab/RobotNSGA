d685eaf909828f37ed4671cbb8f8bb7e66d8da36
Luis Lara <luislarap@outlook.com>
2016-10-05 19:54:26 -0500

    Check if database is valid in graph_module

--- robot_nsga/graph_module.py
+++ robot_nsga/graph_module.py
@@ -1,6 +1,17 @@
 '''Defines the main function of the graph module'''
 
+import os
+import sys
+
+import database
+
 
 def main(args):
 	'''Graph module's main function'''
-	pass
+	abs_path = ''
+	if args.database is None:
+		abs_path = os.getcwd()
+	else:
+		abs_path = os.path.abspath(args.database)
+	if not database.is_database(abs_path, verbose=True):
+		sys.exit()
