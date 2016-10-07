79548ee0ac79d9e5c7575f839426084bf1c6afee
Luis Lara <luislarap@outlook.com>
2016-10-06 19:30:08 -0500

    Add title and labels to the fitness graph

--- robot_nsga/graph/fitness.py
+++ robot_nsga/graph/fitness.py
@@ -20,15 +20,18 @@ def fitness_graph(database, indexes=None):
 	_, axes = plt.subplots(len(indexes), 1, sharex=True, squeeze=False)
 	axes = axes.T[0]
 
-	for j in range(len(indexes)):
+	for j, idx in enumerate(indexes):
 		best_fitness = []
 		worst_fitness = []
 		for i in x_range:
 			database.select(i)
 			individual_data = [val for key, val in database.load_report().items() if key.startswith('I')]
-			gen_fitness = [val['fitness'][indexes[j]] for val in individual_data]
+			gen_fitness = [val['fitness'][idx] for val in individual_data]
 			best_fitness.append(min(gen_fitness))
 			worst_fitness.append(max(gen_fitness))
 		axes[j].semilogy(x_range, best_fitness, 'g', x_range, worst_fitness, 'r')
+		axes[j].set_ylabel(database.properties['objective_names'][idx])
 
+	plt.suptitle('Best and worst fitness')
+	plt.xlabel('Generation')
 	plt.show()
