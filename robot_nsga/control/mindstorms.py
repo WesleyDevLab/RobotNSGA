ec75c67933ae92fdfea8784b22ee6963a219959d
Luis Lara <luislarap@outlook.com>
2016-10-20 12:54:24 -0500

    Create _send_message method in class Mindstorms

--- robot_nsga/control/mindstorms.py
+++ robot_nsga/control/mindstorms.py
@@ -3,8 +3,11 @@
 
 import bluetooth
 
+import numpy as np
+
 from . import robot
 
+HEADER = b'\x01\x00\x81\x9E'
 PORT = 1
 
 class Mindstorms(robot.Robot):
@@ -13,6 +16,23 @@ class Mindstorms(robot.Robot):
 	def __init__(self):
 		self.server_socket = None
 		self.client_socket = None
+
+	def _send_message(self, command, value):
+		'''Sends a command to the robot'''
+		message = HEADER + np.int8(len(command) + 1).tobytes() + \
+			bytes(command, 'ascii') + b'\x00'
+		if isinstance(value, int) or isinstance(value, float):
+			message += np.int16(4).tobytes() + np.float32(value).tobytes()
+		elif isinstance(value, str):
+			message += np.int16(len(value) + 1).tobytes() + bytes(value, 'ascii') + b'\x00'
+		elif isinstance(value, bool):
+			message += np.int16(1).tobytes()
+			if value:
+				message += b'\01'
+			else:
+				message += b'\00'
+		message = np.int16(len(message)).tobytes() + message
+		self.client_socket.send(message)
 
 	def connect(self):
 		'''Connects to an EV3 brick via Bluetooth'''
