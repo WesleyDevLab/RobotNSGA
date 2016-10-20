cea4ab61b25790584b1c3968c037e67b64de0e6f
Luis Lara <luislarap@outlook.com>
2016-10-20 14:45:05 -0500

    Create command interface in manual_ev3

--- robot_nsga/manual_ev3.py
+++ robot_nsga/manual_ev3.py
@@ -1,10 +1,15 @@
 '''Script for manually controlling the EV3 robot'''
 
-import time
-
 import control
 
 ev3 = control.Mindstorms()
 ev3.connect()
-time.sleep(1)
-ev3.disconnect()
+
+while True:
+	command, *value = input().split(' ', maxsplit=1)
+	command = command.upper()
+	if command == 'END':
+		ev3.disconnect()
+		break
+	else:
+		print('Unrecognized command')
