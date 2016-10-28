eee68c194e2b3dbcce222e9e91e20338846c456e
Luis Lara <luislarap@outlook.com>
2016-10-28 17:18:03 -0500

    Create ev3_problem module

---
+++ robot_nsga/ev3_problem.py
@@ -0,0 +1,12 @@
+'''EV3 position regulation using neural networks'''
+
+import evolution
+
+
+class EV3Problem(evolution.Problem):
+	'''Problem class for EV3 robot'''
+	pass
+
+
+def main(args):
+	pass
