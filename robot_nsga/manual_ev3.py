3f3439fa4230fb1b5127c3dad728f5bf7c15aec4
Luis Lara <luislarap@outlook.com>
2016-10-28 16:50:26 -0500

    Add servo commands to manual_ev3 script

--- robot_nsga/manual_ev3.py
+++ robot_nsga/manual_ev3.py
@@ -19,6 +19,12 @@ while True:
 		ev3.set_motor(2, float(value[0]))
 	elif command == 'M3':
 		ev3.set_motor(3, float(value[0]))
+	elif command == 'S1':
+		ev3._send_message('S1', float(value[0]))
+	elif command == 'S2':
+		ev3._send_message('S2', float(value[0]))
+	elif command == 'S3':
+		ev3._send_message('S3', float(value[0]))
 	elif command == 'READ':
 		print(ev3.read_joints())
 	elif command == 'RESET':
